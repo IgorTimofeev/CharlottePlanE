5b9d057b77bbb5e7e89e464ec48e4783f72319fa
Igor Timofeev <methodman1@mail.ru>
2025-12-13 16:07:07 +0300

    pizda1111

--- main/hardware/ahrs/ahrs.h
+++ main/hardware/ahrs/ahrs.h
@@ -36,7 +36,7 @@ namespace pizda {
 				gpio_config_t GPIOConfig {};
 				GPIOConfig.pin_bit_mask = 1ULL << SSPinBitMask;
 				GPIOConfig.mode = GPIO_MODE_OUTPUT;
-				GPIOConfig.pull_up_en = GPIO_PULLUP_ENABLE;
+				GPIOConfig.pull_up_en = GPIO_PULLUP_DISABLE;
 				GPIOConfig.pull_down_en = GPIO_PULLDOWN_DISABLE;
 				GPIOConfig.intr_type = GPIO_INTR_DISABLE;
 				gpio_config(&GPIOConfig);
